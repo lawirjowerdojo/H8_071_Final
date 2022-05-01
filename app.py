eed0c1415f292019f2452ac55ea99aac876e8f01
Levina Wirjowerdojo <99960317+lawirjowerdojo@users.noreply.github.com>
2022-05-01 10:25:38 +0000

    final

--- app.py
+++ app.py
@@ -25,14 +25,5 @@ def predict():
     prediction = f'Prediction: {outcome[model.predict(feature)[0]]}'
     return render_template('main.html', prediction_text=prediction)
 
-# @app.route('/predict/results',methods=['GET'])
-# def predictionresult():
-#     outcome = {
-#         0: 'Did not survive',
-#         1: 'Survived'
-#     }
-#     prediction = outcome[model.predict(feature)[0]]    
-#     return render_template('result.html', prediction_text=prediction)
-
 if __name__ == "__main__":
     app.run(debug=True)
